a6ba15e75cc67f8f62480a0522fd717bde4d560d
coen-h <105825347+coen-h@users.noreply.github.com>
2024-05-10 20:12:44 +1200

    new video, added buttons

--- script.js
+++ script.js
@@ -6,35 +6,25 @@ const greetingElement = document.getElementById("greeting");
 
         let greeting;
         if(hours >= 6 && hours <= 11){
-            greeting ='Good Morning!';
+            greeting ='Good Morning';
         }
         else if(hours >= 12 && hours <= 17){
-            greeting = 'Good Afternoon!';
+            greeting = 'Good Afternoon';
         }
         else if(hours >= 18 && hours <= 21){
-            greeting = 'Good Evening!';
+            greeting = 'Good Evening';
         }
         else if(hours >= 22 && hours <= 5){
-            greeting = 'Good Night!';
+            greeting = 'Good Night';
         }
         else{
             greeting = 'Hello!'
         }
 
     greetingElement.textContent = greeting;
+    setInterval(updateTimeGreeting, 60000);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     updateTimeGreeting();
   });  
-
-const currentTheme = localStorage.getItem("theme");if (currentTheme == "light") {
-    document.body.classList.add("light-theme");
-    }function toggleMode() {
-    document.body.classList.toggle("light-theme");
-    let theme = "dark";
-    if (document.body.classList.contains("light-theme")) {
-    theme = "light";
-    }
-    localStorage.setItem("theme", theme);
-}
