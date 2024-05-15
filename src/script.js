d6aad3bebb726b5baf7413e51e7cae0b39c24b28
coen-h <105825347+coen-h@users.noreply.github.com>
2024-05-15 13:23:24 +1200

    weather widget no mobile support yet

--- src/script.js
+++ src/script.js
@@ -142,3 +142,41 @@ fetch(
         "data:text/html;charset=utf-8;base64," +
         encodeURIComponent(data["content"]);
 });
+let weather = {
+    apiKey: "314701bcbbf912956449b9beb980d4a5",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const temp = data.main.temp.toFixed(1);
+    const { humidity } = data.main;
+    const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+    },
+  };
+  
+  weather.fetchWeather("Auckland");  
