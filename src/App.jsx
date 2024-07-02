ad59737176383408b7e2111664265a35d161c4a6
coen-h <105825347+coen-h@users.noreply.github.com>
2024-07-02 21:04:24 +1200

    initial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Terminal from './components/Terminal'
+
+export default function App() {
+  return (
+    <>
+      <Terminal />
+    </>
+  )
+}
---
+++ src/components/Terminal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Terminal() {
+    return (
+        <div id="terminal">
+            <span id="terminal-input-name">&gt;</span><input id="terminal-input"></input>
+        </div>
+    )
+}
