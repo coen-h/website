75baf75a5557e8701ebb87fb28679a9a0d3d39c4
coen-h <105825347+coen-h@users.noreply.github.com>
2024-07-03 19:00:49 +1200

    terminal look with some commands

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Terminal() {
-    return (
-        <div id="terminal">
-            <span id="terminal-input-name">&gt;</span><input id="terminal-input"></input>
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState([]);
+
+  const handleInput = (event) => {
+    if (event.key === 'Enter') {
+      executeCommand(input.trim());
+      setInput('');
+    }
+  };
+
+  const executeCommand = (command) => {
+    let response = '';
+
+    switch (command.toLowerCase()) {
+      case 'help':
+        response = '- greet\n- time \n- date\n- clear\n- help';
+        break;
+      case 'greet':
+        response = 'Hello there!';
+        break;
+      case 'time':
+        response = new Date().getTime();
+        break;
+      case 'date':
+        response = new Date().toLocaleDateString();
+        break;
+      case 'clear':
+        setOutput([]);
+        return;
+      default:
+        response = `Command not found: ${command}`;
+    }
+
+    setOutput((prevOutput) => [
+      ...prevOutput,
+      { command: command, response: response },
+    ]);
+  };
+
+  return (
+    <div id="terminal">
+      {output.map((item, index) => (
+        <div key={index} className="output">
+          <pre>{`> ${item.command}`}</pre>
+          <pre>{item.response}</pre>
         </div>
-    )
-}
+      ))}
+      <div id="input">
+        <input
+          className="input"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={handleInput}
+        />
+      </div>
+    </div>
+  );
+};
