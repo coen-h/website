eee891a3a39deec5c045d33d552f70c47905a807
coen-h <105825347+coen-h@users.noreply.github.com>
2024-07-10 17:47:17 +1200

    commands working starting site

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Terminal from './components/Terminal'
+import Site from './components/Site'
 
 export default function App() {
   return (
     <>
+      <Site />
       <Terminal />
     </>
   )
---
+++ src/components/Site.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Site() {
+    return (
+        <div id="site" style={{display: "none", justifyContent: "center", alignItems: "center", position: "absolute", width: "80vw", height: "80vh", margin: "10vh 10vw"}}>
+            <p>testing</p>
+        </div>
+    )
+}
