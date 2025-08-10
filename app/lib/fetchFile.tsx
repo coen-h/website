76e7e357bfe1736201efca5df72ceb73462d90ba
coen-h <105825347+coen-h@users.noreply.github.com>
2025-08-10 22:53:35 +1200

    fix file fetching

--- app/lib/fetchFile.tsx
+++ app/lib/fetchFile.tsx
@@ -1,7 +1,7 @@
 "use server";
 
 export default async function fetchFile( repo: string, directory: string[]) {
-  const responseFile = await fetch(`https://api.github.com/repos/coen-h/${repo}/contents${directory.join('/')}`, { next: { revalidate: 3600 } });
+  const responseFile = await fetch(`https://api.github.com/repos/coen-h/${repo}/contents/${directory.join('/')}`, { next: { revalidate: 3600 } });
   const dataFile = await responseFile.json();
 
   return { dataFile };
